10d4082c927dd0507231ec70eb7d95c446b5313c
iperez17 <iperez17@miners.utep.edu>
2020-04-20 09:34:57 -0600

    Updated to be one loop for triangle quiz

--- lcdLib/lcdx.c
+++ lcdLib/lcdx.c
@@ -17,12 +17,9 @@ main()
   clearScreen(COLOR_BLUE);
   
   int j = 0;
-  for(j=0; j < 15; j++)
+  for(j=0; j < 15; j++){
     drawPixel(j, j, COLOR_BLACK);
-
-  for(j=0; j<15; j++)
-    drawPixel(0,j, COLOR_BLACK);
-
-  for(j=0; j<15; j++)
-    drawPixel(j,15, COLOR_BLACK);
+    drawPixel(0, j, COLOR_BLACK);
+    drawPixel(j, 15, COLOR_BLACK);
+  }
 }
